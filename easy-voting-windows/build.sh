bbdcee0a5136f678c6aa7ecc23ee741d05c4af8b
pilinsin <84775765+pilinsin@users.noreply.github.com>
2022-05-15 21:16:55 +0900

    this app is dependent on i2p

--- easy-voting-windows/build.sh
+++
@@ -1,8 +0,0 @@
-fyne package -os windows -icon ../Icon.png
-tar xJvf easy-voting-windows.tar.xz
-mv usr/local easy-voting-windows
-echo ./bin/easy-voting-windows >> easy-voting-windows/launch.sh
-chmod +711 easy-voting-windows/launch.sh
-
-tar cJvf easy-voting-windows.tar.xz easy-voting-windows
-rm -r usr easy-voting-windows Makefile
